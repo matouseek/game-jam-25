636ca85920ae8cd1f2690a06c9284d94273baba6
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-28 22:56:03 +0100

    Add initial architecture

---
+++ scripts/game_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+const PATH_TO_MINIGAMES = "res://scenes/" 
+const SCENES := ["minigames/empty.tscn"] ## contains filenames for scenes in sequential order
+
+signal level_completed ## each minigame sends it when done
+
+var current_level : int = 0 ## tracks the current game state
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	level_completed.connect(next_level)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# changes tracker to next position
+func next_level() -> void:
+	current_level += 1
+
+# TODO: loads the current scene, fade in/out
+func load_level() -> void:
+	pass
