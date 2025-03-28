2f6d35d725cbc97269098f595c8c4f5845166eb7
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-29 00:51:01 +0100

    Remove unnecessary text

--- scripts/2_shooting_balls.gd
+++ scripts/2_shooting_balls.gd
@@ -4,7 +4,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Button.button_down.connect(func() : GM.level_completed.emit())
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
