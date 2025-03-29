81bc40c7eac5de23f0549ef73a9f1e1eae414c77
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-29 09:58:44 +0100

    Add function

--- scripts/zeroland.gd
+++ scripts/zeroland.gd
@@ -1,10 +1,13 @@
 extends Node2D
-const SLIDE_TIMES : Array = [1,2]
-const TRANSITIONS_POSITIONS : Array = [Vector2(392,84),Vector2(536,84)]
-const TRANSITION_TIME : float = 0.5
+const SLIDE_TIMES : Array = [1,2] # first number - how long the 0th image is there
+const TRANSITIONS_POSITIONS : Array = [Vector2(392,84),Vector2(536,84)] # centers of the comics images
+const TRANSITION_TIME : float = 0.5 # how long each transition takes
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_transitions()
+
+func start_transitions():
 	for i in range(len(SLIDE_TIMES)):
 		var t = SLIDE_TIMES[i]
 		var p = TRANSITIONS_POSITIONS[i]
