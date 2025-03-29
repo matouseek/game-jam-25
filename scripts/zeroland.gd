7f6cf4a6709e31af71915315d9551683f31effe9
SADOVSKV <3217cody@gmail.com>
2025-03-29 08:39:44 +0100

    animation somehow done

--- scripts/zeroland.gd
+++ scripts/zeroland.gd
@@ -1,12 +1,14 @@
 extends Node2D
-
+const SLIDE_TIMES : Array = [1,2]
+const TRANSITIONS_POSITIONS : Array = [Vector2(396,84),Vector2(542,84)]
+const TRANSITION_TIME : float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$AnimatedSprite2D.
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	for i in range(len(SLIDE_TIMES)):
+		var t = SLIDE_TIMES[i]
+		var p = TRANSITIONS_POSITIONS[i]
+		var timer = get_tree().create_timer(t)
+		await timer.timeout
+		var tween = get_tree().create_tween()
+		tween.tween_property($Camera,"position", p, TRANSITION_TIME)
