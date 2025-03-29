5e2972dc0ecce86d82e821684e68bec93acda2ed
SADOVSKV <3217cody@gmail.com>
2025-03-29 09:24:26 +0100

    polish

--- scripts/zeroland.gd
+++ scripts/zeroland.gd
@@ -1,6 +1,6 @@
 extends Node2D
 const SLIDE_TIMES : Array = [1,2]
-const TRANSITIONS_POSITIONS : Array = [Vector2(396,84),Vector2(542,84)]
+const TRANSITIONS_POSITIONS : Array = [Vector2(392,84),Vector2(536,84)]
 const TRANSITION_TIME : float = 0.5
 
 # Called when the node enters the scene tree for the first time.
