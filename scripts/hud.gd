5912c33973043a1fc87d9ab175b0d928796ac65f
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-29 13:30:38 +0100

    Improve hud button texture loading

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -22,9 +22,9 @@ func _ready():
 func initialize_button_icons():
 	for i in $Buttons.get_child_count():
 		var butt = $Buttons.get_child(i) as TextureButton
-		butt.texture_normal = ImageTexture.create_from_image(Image.load_from_file(PATH_TO_BUTT_ICONS + "default" + str(i) + ".png"))
-		butt.texture_hover = ImageTexture.create_from_image(Image.load_from_file(PATH_TO_BUTT_ICONS + "hover" + str(i) + ".png"))
-		butt.texture_pressed = ImageTexture.create_from_image(Image.load_from_file(PATH_TO_BUTT_ICONS + "pressed" + str(i) + ".png"))
+		butt.texture_normal = load(PATH_TO_BUTT_ICONS + "default" + str(i) + ".png")
+		butt.texture_hover = load(PATH_TO_BUTT_ICONS + "hover" + str(i) + ".png")
+		butt.texture_pressed = load(PATH_TO_BUTT_ICONS + "pressed" + str(i) + ".png")
 
 # connects to the buttons pressed signals
 func connect_buttons():
