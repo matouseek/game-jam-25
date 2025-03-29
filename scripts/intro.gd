7df7ab2d6e6b6e5053f2801f4d28a172b40fc28d
stepanvodsedalek <stepanvodsedalek@gmail.com>
2025-03-29 17:16:48 +0100

    tweaked parallax, added assests

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -4,5 +4,5 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property($Camera2D, "position", Vector2(0,0), 5).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property($Camera2D, "position", Vector2(0,0), 7).set_trans(Tween.TRANS_CUBIC)
 	
