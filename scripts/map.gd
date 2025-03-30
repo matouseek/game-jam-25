bf78678cabb0ccf9c6ea338f6bec7866ee260add
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-30 15:15:01 +0200

    Improve map

--- scripts/map.gd
+++ scripts/map.gd
@@ -8,10 +8,17 @@ const ICON_RED : String = "res://assets/icon_red.svg"
 
 @onready var curves = $Curves
 
-const SHIP_MOVE_TIME = 1.0
+const SHIP_ICON = "res://assets/map/ship_small.png"
+const SHIP_ICON_SCALE = 0.5
+const SHIP_MOVE_TIME = 2.0
+const SHIP_BOTTOM_OFFSET = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if not GM.travelling_back:
+		await add_ship_to_path(GM.current_level,0.0,1.0)
+	else:
+		await add_ship_to_path(GM.current_level+1,1.0,0.0)
 	#$NextButton.pressed.connect(func() : GM.map_completed.emit())
 	$NextButton.pressed.connect(move_ship_next)
 	%CurrentLevel.text = "current: " + str(GM.current_level)
@@ -24,14 +31,25 @@ func prep_icons():
 	for u in uncasted:
 		icons.append(u as TextureRect)
 
-func next_travel(index : int, start_ratio : float, end_ratio : float):
+# adds ship to path to be visible right at the start of the scene
+func add_ship_to_path(index : int, start_ratio : float, end_ratio : float):
 	var curve = curves.get_child(index)
 	var path_follow = curve.get_node("PathFollow2D") as PathFollow2D
+	path_follow.rotates = false
+	path_follow.rotation = 0
 	var sprite = Sprite2D.new()
-	sprite.texture = load("res://assets/icon.svg")
+	sprite.texture = load(SHIP_ICON)
+	sprite.apply_scale(Vector2(SHIP_ICON_SCALE,SHIP_ICON_SCALE))
+	sprite.position.y -= SHIP_BOTTOM_OFFSET
+	if GM.travelling_back:
+		sprite.flip_h = true
 	path_follow.add_child(sprite)
-	# chatgpt generated
 	path_follow.progress_ratio = start_ratio
+
+# start moving the ship to the next point
+func tween_path_follow(index : int, start_ratio : float, end_ratio : float):
+	var curve = curves.get_child(index)
+	var path_follow = curve.get_node("PathFollow2D") as PathFollow2D
 	var tween = get_tree().create_tween()
 	tween.tween_property(path_follow, "progress_ratio", end_ratio, SHIP_MOVE_TIME)
 	await tween.finished
@@ -40,9 +58,9 @@ func next_travel(index : int, start_ratio : float, end_ratio : float):
 func move_ship_next():
 	$NextButton.disabled = true
 	if not GM.travelling_back:
-		await next_travel(GM.current_level,0.0,1.0)
+		await tween_path_follow(GM.current_level,0.0,1.0)
 	else:
-		await next_travel(GM.current_level+1,1.0,0.0)
+		await tween_path_follow(GM.current_level+1,1.0,0.0)
 	GM.map_completed.emit()
 	
 func set_icon_colors_forwards():
